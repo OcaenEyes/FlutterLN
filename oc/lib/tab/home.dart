21ef1b711cff5d0a886d0fc3f7772d024011cd1f
OcaenEyes <1450136519@qq.com>
2019-02-06 19:22:25 +0800

    master
    master

--- oc/lib/tab/home.dart
+++ oc/lib/tab/home.dart
@@ -65,7 +65,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
 
                   Container(
                     width: 335,
-                    height: 240,
+                    height: 200,
                     margin: const EdgeInsets.only(left: 20,right: 20),
                     // padding: const EdgeInsets.only(top: 8,bottom:8,left: 18),
                     // color: Colors.green,
@@ -88,7 +88,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
 
             Container(
               width: 375,
-              height: 80,
+              height: 160,
               // color: Colors.pink,
               child: new GridView.count(
                 padding: const EdgeInsets.all(20),
@@ -98,9 +98,10 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                   new Container(
                     child: Column(
                       children: <Widget>[
-                        Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height: 24,fit: BoxFit.cover,alignment: Alignment.center,),
-                        
-                        Text('一',),
+                        // Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height: 24,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Image.asset('images/personalized.png',width: 20,height: 20,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Padding(padding: EdgeInsets.all(6.0),),
+                        Text('内容一',textAlign: TextAlign.center),
                       ],
                     ),
                   
@@ -109,9 +110,10 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                   new Container(
                     child: Column(
                       children: <Widget>[
-                        Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height:24, fit: BoxFit.cover,alignment: Alignment.center,),
-                        
-                        Text('二'),
+                        // Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height:24, fit: BoxFit.cover,alignment: Alignment.center,),
+                        Image.asset('images/personalized.png',width: 20,height: 20,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Padding(padding: EdgeInsets.all(6.0),),
+                        Text('内容二',textAlign: TextAlign.center),
                       ],
                     ),
                   
@@ -120,8 +122,10 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                   new Container(
                     child: Column(
                       children: <Widget>[
-                        Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height: 24,fit: BoxFit.cover,alignment: Alignment.center,),
-                        Text('三',),
+                        // Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height: 24,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Image.asset('images/personalized.png',width: 20,height: 20,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Padding(padding: EdgeInsets.all(6.0),),
+                        Text('内容三',textAlign: TextAlign.center),
                       ],
                     ),
                   
@@ -130,19 +134,149 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                   new Container(
                     child: Column(
                       children: <Widget>[
-                        Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height: 24,fit: BoxFit.cover,alignment: Alignment.center,),
-                        Text('四'),
+                        // Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height: 24,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Image.asset('images/personalized.png',width: 20,height: 20,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Padding(padding: EdgeInsets.all(6.0),),
+                        Text('内容四',textAlign: TextAlign.center),
                       ],
                     ),
                   
                   ),
  
 
+                  new Container(
+                    child: Column(
+                      children: <Widget>[
+                        // Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height: 24,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Image.asset('images/personalized.png',width: 20,height: 20,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Padding(padding: EdgeInsets.all(6.0),),
+                        Text('内容一',textAlign: TextAlign.center),
+                      ],
+                    ),
+                  
+                  ),
+
+                  new Container(
+                    child: Column(
+                      children: <Widget>[
+                        // Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height:24, fit: BoxFit.cover,alignment: Alignment.center,),
+                        Image.asset('images/personalized.png',width: 20,height: 20,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Padding(padding: EdgeInsets.all(6.0),),
+                        Text('内容二',textAlign: TextAlign.center),
+                      ],
+                    ),
+                  
+                  ),
+
+                  new Container(
+                    child: Column(
+                      children: <Widget>[
+                        // Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height: 24,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Image.asset('images/personalized.png',width: 20,height: 20,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Padding(padding: EdgeInsets.all(6.0),),
+                        Text('内容三',textAlign: TextAlign.center),
+                      ],
+                    ),
+                  
+                  ),
+
+                  new Container(
+                    child: Column(
+                      children: <Widget>[
+                        // Image.network('https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1549421232877&di=b891bc0770d9382be1bd9c4fbdbbad15&imgtype=0&src=http%3A%2F%2Fimage.tupian114.com%2F20140424%2F09361709.png',width: 24,height: 24,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Image.asset('images/personalized.png',width: 20,height: 20,fit: BoxFit.cover,alignment: Alignment.center,),
+                        Padding(padding: EdgeInsets.all(6.0),),
+                        Text('内容四',textAlign: TextAlign.center),
+                      ],
+                    ),
+                  
+                  ),
+ 
 
                   
 
                 ],
 
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.only(left: 20.0),
+              child:Text('今日推荐',style: TextStyle(color: Colors.black,fontSize: 24,letterSpacing: 2.0,fontWeight: FontWeight.bold)), 
+            ),
+
+
+            
+            Padding(padding: EdgeInsets.only(top: 6.0,bottom: 6.0),),
+
+            Container(
+              width: 375,
+              height: 180,
+              padding: EdgeInsets.only(left:20.0,right: 20.0),
+              // color: Colors.red,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    width: 180,
+                    height: 180,
+              
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                      image: DecorationImage(
+                        image: NetworkImage('http://www.deskier.com/uploads/allimg/160817/1-160QH13515.jpg'),
+                        fit: BoxFit.cover,
+
+                      ),
+                    ),
+                    // color: Colors.black54,
+                    
+                  ),
+
+                  Container(
+                    width: 195,
+                    height: 180,
+                    // padding: EdgeInsets.all(6.0),
+                    // color: Colors.orangeAccent,
+                    child: Column(
+                      // scrollDirection: Axis.vertical,
+                      
+                      children: <Widget>[
+                        
+                        Container(
+                          width: 175,
+                          height: 88,
+                          margin: EdgeInsets.only(bottom: 4.0),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                            image: DecorationImage(
+                              image: NetworkImage('http://attachments.gfan.com/forum/201503/14/02541368qtpd7hqddp43np.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+
+                        
+                        ),
+
+                        Container(
+                          width: 175,
+                          height: 88,
+                           decoration: BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                            image: DecorationImage(
+                              
+                              image: NetworkImage('http://b.zol-img.com.cn/desk/bizhi/image/4/1920x1080/1395366007267.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+
+                          
+                        ),
+                      ],
+                    ),
+                  ),
+
+                ],
               ),
             ),
 
--- oc/lib/tab/mine.dart
+++ oc/lib/tab/mine.dart
@@ -9,6 +9,46 @@ class _MinePageWidgetState extends State<MinePageWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('我的'),),
+      body: ListView(
+          children: <Widget>[
+
+            new ListTile(
+              title: Text('条目一'),
+              leading: Icon(Icons.pages),
+              trailing: Text('说明一'),
+            ),
+
+            new ListTile(
+              title: Text('条目一'),
+              leading: Icon(Icons.pages),
+              trailing: Text('说明一'),
+            ),
+
+            new ListTile(
+              title: Text('条目一'),
+              leading: Icon(Icons.pages),
+              trailing: Text('说明一'),
+            ),
+
+            new ListTile(
+              title: Text('条目一'),
+              leading: Icon(Icons.pages),
+              trailing: Text('说明一'),
+            ),
+
+
+            new ListTile(
+              title: Text('条目一'),
+              leading: Icon(Icons.pages),
+              trailing: Text('说明一'),
+            ),
+
+          ],
+          
+        ),
+
+        
     );
   }
 }
