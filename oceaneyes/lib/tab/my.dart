384504a2089214ecd805ddb6a8c33b052ee5155e
OcaenEyes <1450136519@qq.com>
2019-12-04 23:32:17 +0800

    Update my.dart

--- oceaneyes/lib/tab/my.dart
+++ oceaneyes/lib/tab/my.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:oceaneyes/my/myPage.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyWidget extends StatefulWidget {
   @override
@@ -8,10 +9,16 @@ class MyWidget extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<MyWidget> {
+  
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750,height: 1334,allowFontScaling: true)..init(context);
     return Scaffold(
-      body: MyPagewidget(),
+      body:new Container(
+        child: MyPagewidget(),
+        width: ScreenUtil.getInstance().setWidth(750),
+        height: ScreenUtil.getInstance().setHeight(1334),
+      ) ,
     );
   }
 }
