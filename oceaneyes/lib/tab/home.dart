2448a42ce000268df2068cd9e26e0a922066b0d3
OcaenEyes <1450136519@qq.com>
2019-12-04 23:32:14 +0800

    Update home.dart

--- oceaneyes/lib/tab/home.dart
+++ oceaneyes/lib/tab/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:oceaneyes/home/homePage.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeWidget extends StatefulWidget {
   @override
@@ -11,8 +12,13 @@ class _HomeWidgetState extends State<HomeWidget> {
   @override
 
   Widget build(BuildContext context) {
+    ScreenUtil.instance = ScreenUtil(width: 750,height: 1334,allowFontScaling: true)..init(context);
     return Scaffold(
-      body: HomePageWidget(),
+      body: Container(
+        child: HomePageWidget(),
+        width: ScreenUtil.getInstance().setWidth(750),
+        height: ScreenUtil.getInstance().setHeight(1334),
+        ),
     );
   }
 
