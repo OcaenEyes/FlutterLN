9ad0da22fef98393c90648520cadb1de64c539e7
OcaenEyes <1450136519@qq.com>
2019-12-07 14:14:35 +0800

    Update myPage.dart

--- oceaneyes/lib/my/myPage.dart
+++ oceaneyes/lib/my/myPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class MyPagewidget extends StatefulWidget {
   @override
@@ -6,25 +7,31 @@ class MyPagewidget extends StatefulWidget {
 
 }
 
-class _MyPageWidgetState extends State<MyPagewidget> {
+class _MyPageWidgetState extends State<MyPagewidget> { 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: true,
-            expandedHeight: 250.0,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.asset("assets/images/bg.jpg",fit: BoxFit.cover,),
-              title: new Text("呵呵呵"),
-            ),
-          ),
-
-          
-        ],
+      appBar: AppBar(
+        title: Text("我的"),
       ),
+      body: new ListView(
+
+      )
 
     );
+  }
+}
+
+Future getHttp()async {
+  try{
+    Response response;
+    Dio dio = new Dio();
+    response  = await dio.get("http://www.baidu.com");
+    print(response);
+    return response.data;
+
+  }
+  catch(e){
+    return debugPrint(e+"异常了");
   }
 }
