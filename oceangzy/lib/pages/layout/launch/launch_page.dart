be31b6532e251aa0418d721be5cbc368e4d6f9be
OcaenEyes <1450136519@qq.com>
2020-07-22 02:09:03 +0800

    master

---
+++ oceangzy/lib/pages/layout/launch/launch_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LaunchPage extends StatefulWidget {
+  _LaunchPageState createState() => _LaunchPageState();
+}
+
+class _LaunchPageState extends State<LaunchPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Text("你好 "),
+    );
+  }
+}
--- oceangzy/lib/router/page_routes.dart
+++ oceangzy/lib/router/page_routes.dart
@@ -1,18 +1,28 @@
 import 'package:oceangzy/pages/tab/bottom_tab_bar.dart';
 import 'package:oceangzy/pages/layout/information/information_page.dart';
 import 'package:oceangzy/pages/layout/home/home_page.dart';
+import 'package:oceangzy/pages/layout/launch/launch_page.dart';
 import 'package:oceangzy/pages/layout/common/single_image_view_page.dart';
 import 'package:oceangzy/pages/layout/common/gallery_image_view_page.dart';
 import 'package:oceangzy/router/page_builder.dart';
 
-enum PageName { bottom_tab, information_page, home_page, single_image,gallery_image }
+enum PageName {
+  launch_page,
+  bottom_tab,
+  information_page,
+  home_page,
+  single_image,
+  gallery_image
+}
 
 final Map<PageName, PageBuilder> pageRoutes = {
   PageName.bottom_tab: PageBuilder(builderFunc: (bundle) => BottomTabBar()),
+  PageName.launch_page: PageBuilder(builderFunc: (bundle) => LaunchPage()),
   PageName.information_page:
       PageBuilder(builderFunc: (bundle) => InformationPage()),
   PageName.single_image:
       PageBuilder(builderFunc: (bundle) => SingleImageViewPage(bundle)),
-      PageName.gallery_image:PageBuilder(builderFunc: (bundle) => GalleryImageViewPage(bundle)),
+  PageName.gallery_image:
+      PageBuilder(builderFunc: (bundle) => GalleryImageViewPage(bundle)),
   PageName.home_page: PageBuilder(builderFunc: (bundle) => HomePage()),
 };
